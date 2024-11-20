206dceb937c50e35736807b7f7c791cc60359d5e
Sebas Cruz <sebas.cruz750@gmail.com>
2024-11-20 12:55:26 -0500

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import { Repo } from "./interfaces/index";
-
 const API_URL = "https://api.github.com/orgs/stackbuilders/repos";
 
+// API CALL
 const callApiService = (): Promise<Repo[]> => {
   return fetch(API_URL)
     .then((response) => response.json())
     .catch((error) => alert(error));
 };
 
+// MORE THAN 5 STARS
 const filterRepoByStars = (starsNumber: number, repoList: Repo[]) => {
   if (repoList.length === 0) {
     return repoList;
@@ -17,34 +18,30 @@ const filterRepoByStars = (starsNumber: number, repoList: Repo[]) => {
   });
 };
 
-const showRepoInfo = (repoList: Repo[]) => {
-  const stars = document.getElementById("stars");
+const showRepoFiveStars = (repoList: Repo[], htmlEl: string) => {
+  const ele = document.getElementById(htmlEl);
   repoList.map(({ name, stargazers_count, html_url }) => {
     const repoEl = document.createElement("li");
     repoEl.innerHTML = `
     <a href="${html_url}">
     ${name}
-    </a>
-    tiene ${stargazers_count} estrellas `;
-    stars?.appendChild(repoEl);
+    </a> - ${stargazers_count}`;
+    ele?.appendChild(repoEl);
   });
 };
 
-const filterRepoDates = (repos: Repo[] ) => {
+// MOST RECENT UPDATED REPOS
+const filterRepoByDates = (repos: Repo[]) => {
   return repos
     .sort((a, b) => {
       return (
-        new Date(b.updated_at).getTime() -
-        new Date(a.updated_at).getTime()
+        new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()
       );
     })
     .slice(0, 5);
 };
 
-// hapistrano, dotenv, atomic, openssh, inflections
-// twitter, atomic, dotenv, dbcleaner, openssh
-
-const showLastReposUpdated = (repos: Repo[]) => {
+const showLastUpdatedRepos = (repos: Repo[]) => {
   const updated = document.getElementById("updated");
   repos.map(({ name, updated_at, html_url }) => {
     const dateEl = document.createElement("li");
@@ -52,13 +49,14 @@ const showLastReposUpdated = (repos: Repo[]) => {
     <a href="${html_url}"> 
     ${name}  
     </a>
-    fue actualizado el ${updated_at.toString().split("-")[1]} - ${
+    <br/> ${updated_at.toString().split("-")[1]} - ${
       updated_at.toString().split("-")[0]
     } `;
     updated?.appendChild(dateEl);
   });
 };
 
+// ALL STARS COUNT
 const getRepoStarsCount = (repos: Repo[]) => {
   let starsCount = 0;
   repos.map((repo) => {
@@ -78,30 +76,72 @@ const showAllRepoStars = (starsCount: number) => {
   allStars?.appendChild(totalStars);
 };
 
+// TOP STARS REPO
+const getTopRepos = (repoList: Repo[]) => {
+  const topRepo = filterRepoByStars(5, repoList);
+  return topRepo
+    .sort((a: Repo, b: Repo) => a.stargazers_count - b.stargazers_count)
+    .reverse()
+    .slice(0, 5);
+};
+// for displaying in HTML we use the showRepoFiveStar function
+
+// NO-H REPOS
+const getNoHRepos = (repoList: Repo[]) => {
+  return repoList.filter(({ name }) => {
+    return name[0] !== "h";
+  });
+};
+
+const showNoHRepos = (repoList: Repo[]) => {
+  const htmlEl = document.getElementById("noH");
+  repoList.map(({ name }) => {
+    const repoName = document.createElement("li");
+    repoName.innerHTML = name;
+    htmlEl?.appendChild(repoName);
+  });
+};
+
+// GENERAL FUNCTIONS
 const getRepoStars = () => {
   callApiService().then((repos) => {
-    showRepoInfo(filterRepoByStars(5, repos));
+    showRepoFiveStars(filterRepoByStars(5, repos), "stars");
   });
 };
 
 const getRepoLastUpdated = () => {
   callApiService().then((repos) => {
-    showLastReposUpdated(filterRepoDates(repos));
+    showLastUpdatedRepos(filterRepoByDates(repos));
   });
 };
+
 const getAllStars = () => {
   callApiService().then((repos) => {
     showAllRepoStars(getRepoStarsCount(repos));
   });
 };
 
+const getTopFiveRepo = () => {
+  callApiService().then((repos) => {
+    showRepoFiveStars(getTopRepos(repos), "topFive");
+  });
+};
+
+const getReposWithoutH = () => {
+  callApiService().then((repos) => {
+    showNoHRepos(getNoHRepos(repos));
+  });
+};
+
+// FUNCTION CALLS
 getRepoStars();
 getRepoLastUpdated();
 getAllStars();
+getTopFiveRepo();
+getReposWithoutH();
 
 // module.exports = {
 //   filterRepoByStars,
 //   filterRepoDates,
 //   getRepoStarsCount,
 // };
-
